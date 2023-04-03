de8a4c49105dd644b1e3805b7cb550bf47463cef
Ian Rumac <ian.rumac@outlook.com>
2023-04-03 12:53:32 +0200

    Fix CountAction tests,add IR compiler for JS, remove swiftpackage

--- src/commonTest/kotlin/com/ianrumac/redukks/util.kt
+++ src/commonTest/kotlin/com/ianrumac/redukks/util.kt
@@ -4,7 +4,7 @@ import com.ianrumac.redukks.defaults.BasicReducedStore
 
 fun testContext(startCount: Int = 0) =
     object : CountContext {
-      override val countStore: CountStore = BasicReducedStore(CountContext.State(0))
+      override val countStore: CountStore = BasicReducedStore(CountContext.State(startCount))
       override val countClient: CountClient =
           object : CountClient {
             override suspend fun add(number: Int): Int = countStore.state.total + number
